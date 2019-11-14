fb40ec40b7bf19c8501dd894642feb55ed793101
riha <Richa1998.patel@gmail.com>
2019-11-14 17:48:21 -0500

    WITH CPA CALCULATION.->

--- Ricah_764947_gapApp/CalculateGpa.swift
+++ Ricah_764947_gapApp/CalculateGpa.swift
@@ -22,25 +22,60 @@ class CalculateGpa: UIViewController {
     @IBOutlet weak var course4_txt: UITextField!
     @IBOutlet weak var course5_txt: UITextField!
     
+    var student_grades : [Double] = []
     var total : Double = 0
+    var stud_final_gpa : Double = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        for i in 1...4{
-            
-            //var temp_count = 
-            
-        }
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func calculate(_ sender: Any) {
        
+        var credits : [Double] = []
+        var total_credit : Double = 0
+        
+        credits = [get_credit(Course: course1.text!),get_credit(Course: course2.text!),get_credit(Course: course3.text!),get_credit(Course: course4.text!),get_credit(Course: course5.text!)]
+      
+        student_grades = [credit_counter(credits: Int(course1_txt.text!)!) ,credit_counter(credits: Int(course2_txt.text!)!),credit_counter(credits: Int(course3_txt.text!)!),credit_counter(credits: Int(course4_txt.text!)!),credit_counter(credits: Int(course5_txt.text!)!)]
+        
+     
+        for i in 0...4{
+            
+            let temp = Double(credits[i]) * student_grades[i]
+            total_credit += credits[i]
+            total += temp
+            
+            
+        }
+        
+      print(total/total_credit)
+
         
     }
-    func get_credit(Course : String) -> Int {
+    func get_credit(Course : String) -> Double {
          
         let temp : String = String(Course.dropFirst(Course.count - 1))
-        return Int(temp) ?? 0
+        return Double(temp) ?? 0
+        
+    }
+    
+    func credit_counter(credits : Int) -> Double {
+        switch credits {
+            case 94...100 : return 4
+            case 84...93 : return 3.7
+            case 80...86 : return 3.5
+            case 77...79 : return 3.2
+            case 73...76 : return 3.0
+            case 70...72 : return 2.7
+            case 67...69 : return 2.3
+            case 63...66 : return 2.0
+            case 60...32 : return 1.7
+            case 50...59 : return 1.0
+            case 49...0  : return 0.0
+            default      :  return 0
+        }
         
     }
     
